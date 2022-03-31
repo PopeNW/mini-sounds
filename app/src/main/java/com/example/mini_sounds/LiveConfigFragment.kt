7866990f65928daf3efd11aba9dad1548c8fe4d4
Nathan Pope <nathan.pope@bbc.co.uk>
2022-03-31 17:41:42 +0100

    Added Coil library for loading images. Added playable item image to live config fragment. Changed live config title text to playable item primary title.

--- app/src/main/java/com/example/mini_sounds/LiveConfigFragment.kt
+++ app/src/main/java/com/example/mini_sounds/LiveConfigFragment.kt
@@ -1,13 +1,13 @@
 package com.example.mini_sounds
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.example.mini_sounds.databinding.ActivityMainBinding
-import com.example.mini_sounds.databinding.FragmentKilledConfigBinding
+import coil.load
 import com.example.mini_sounds.databinding.FragmentLiveConfigBinding
 
 class LiveConfigFragment : Fragment() {
@@ -31,9 +31,20 @@ class LiveConfigFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val rms = viewModel.rms.value
+
         if (rms != null) {
-            binding.textView2.text = rms.modules[0].title
+            with(binding) {
+                val imageUrl =
+                    getImageWithRecipe(rms.modules[0].playableItems[0].imageUrl)
+
+                playableItemTitle.text = rms.modules[0].playableItems[0].titles.primary
+                playableItemImage.load(imageUrl)
+            }
         }
+    }
+
+    private fun getImageWithRecipe(imageUrl: String, imageSize: String = "320x320"): String {
+        return imageUrl.replace("{recipe}", imageSize)
     }
 
     override fun onDestroyView() {
